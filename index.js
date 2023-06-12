4dc1655248214b9b4044e8a297b70c381129d1d2
Victor <victornjoroge2020@gmail.com>
2023-06-12 09:01:42 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerright() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  moveDodgerright();
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left -1}px`;
+    }
+  }
+  moveDodgerLeft();
